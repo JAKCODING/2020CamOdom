cba6e670100163f5f2858a76a90f7974406ec204
JAKCODING <jtofounder@gmail.com>
2020-08-09 18:59:13 -0400

    BULK READ WORKS!!!! YESSDSKFJSDLKFJDSLKFJF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/BulkReadTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/BulkReadTest.java
@@ -3,15 +3,19 @@ package org.firstinspires.ftc.teamcode.test;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.lynx.LynxUsbDevice;
 import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataResponse;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.util.BulkRead;
 import org.openftc.revextensions2.ExpansionHubEx;
+import org.openftc.revextensions2.RevBulkData;
 
+@Autonomous(name="bulkread")
 public class BulkReadTest extends OpMode {
 
     LynxUsbDevice lUsb;
+    ExpansionHubEx expX;
     LynxModule lMod;
     BulkRead bRead;
     LynxGetBulkInputDataResponse lResponse;
@@ -20,16 +24,29 @@ public class BulkReadTest extends OpMode {
     public void init() {
 
         lUsb = (LynxUsbDevice) hardwareMap.get("Expansion Hub Portal 1");
-        lMod = new LynxModule(lUsb, 2, true);
+        lMod = hardwareMap.get(LynxModule.class, "Control Hub");
+        expX = hardwareMap.get(ExpansionHubEx.class, "Control Hub");
         lResponse = new LynxGetBulkInputDataResponse(lMod);
 
+        encY = hardwareMap.dcMotor.get("encY");
+        encX1 = hardwareMap.dcMotor.get("encX");
 
-        bRead = new BulkRead(lResponse, lMod, encX1, encX2, encY);
+
+        bRead = new BulkRead(lMod, encX1, encX2, encY, new int[]{0});
 
     }
 
 
     public void loop() {
-        //Woo
+        lMod.setConstant(532342);
+
+        double[] retVal = bRead.getMotors();
+        String readVal = retVal[0] + " " + retVal[1];
+
+        telemetry.addData("String return: ", readVal);
+        telemetry.addLine(expX.getConnectionInfo());
+        telemetry.addData("String return: ", expX.getBulkInputData().getMotorCurrentPosition(bRead.portX1));
+        telemetry.addData("X port: ", bRead.portX1);
+        telemetry.addData("Y port: ", bRead.portY);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/BulkRead.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/BulkRead.java
@@ -1,23 +1,33 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.qualcomm.hardware.lynx.LynxI2cDeviceSynchV1;
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataCommand;
 import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataResponse;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.I2cController;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchImplOnSimple;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchSimple;
 
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.openftc.revextensions2.ExpansionHubEx;
 import org.openftc.revextensions2.RevBulkData;
+
+import java.util.ArrayList;
 
 public class BulkRead {
 
-    LynxGetBulkInputDataResponse lynxResponse;
     LynxModule controlHub;
+    ArrayList<I2cDeviceSynch> i2cRead;
+
 
     DcMotor x1, x2, y;
-    int portX1, portX2, portY;
+    public int portX1, portX2, portY;
 
-    public BulkRead(LynxGetBulkInputDataResponse lynxResponse, LynxModule controlHub, DcMotor x1, DcMotor x2, DcMotor y) {
+    public BulkRead(LynxModule controlHub, DcMotor x1, DcMotor x2, DcMotor y, int[] i2cPorts) {
 
-        this.lynxResponse = lynxResponse;
         this.controlHub = controlHub;
 
         this.x1 = x1;
@@ -25,28 +35,65 @@ public class BulkRead {
         this.y = y;
 
         portX1 = this.x1.getPortNumber();
-        portX2 = this.x2.getPortNumber();
+        //portX2 = this.x2.getPortNumber();
         portY = this.y.getPortNumber();
 
-    }
+        /*for(int i = 0; i < i2cPorts.length; i++) {
+            i2cRead.add(new BetterI2cDeviceSynchImplOnSimple(
+                    new LynxI2cDeviceSynchV1(AppUtil.getDefContext(), controlHub, i2cPorts[i]), true));
+        }*/
 
-    public String bulkResponse() {
-
-        return lynxResponse.toString();
-
-    }
-
-    public double[] getMotors() {
-
-        double posOne = (x1.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(portX1) : lynxResponse.getEncoder(portX1);
-        double posTwo = (x2.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(portX2) : lynxResponse.getEncoder(portX2);
-        double posThree = (y.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(portY) : lynxResponse.getEncoder(portY);
-
-        return new double[]{posOne, posTwo, posThree};
 
     }
 
-    public double[] getGenericMotors(DcMotor d1, DcMotor d2, DcMotor d3, DcMotor d4) {
+    public void armI2C(int heartbeatMs) {
+
+        for(int i = 0; i < i2cRead.size(); i++) {
+
+            i2cRead.get(i).setHeartbeatInterval(heartbeatMs);
+
+        }
+
+    }
+
+
+    public synchronized double [] getMotors() {
+
+        LynxGetBulkInputDataCommand command = new LynxGetBulkInputDataCommand(controlHub);
+        try
+        {
+            LynxGetBulkInputDataResponse lynxResponse = command.sendReceive();
+            double posOne = (x1.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(portX1) : lynxResponse.getEncoder(portX1);
+            //double posTwo = (x2.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(portX2) : lynxResponse.getEncoder(portX2);
+            double posThree = (y.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(portY) : lynxResponse.getEncoder(portY);
+            return new double[]{posOne/*, posTwo*/, posThree};
+        }
+
+        catch (Exception e)
+        {
+            return new double[]{943028, 932432908};
+        }
+
+
+
+
+
+
+    }
+
+    public synchronized double[] getGenericMotors(DcMotor d1, DcMotor d2, DcMotor d3, DcMotor d4) {
+
+        LynxGetBulkInputDataCommand command = new LynxGetBulkInputDataCommand(controlHub);
+        LynxGetBulkInputDataResponse lynxResponse;
+        try
+        {
+            lynxResponse = command.sendReceive();
+        }
+
+        catch (Exception e)
+        {
+            return new double[]{32498329, 239493};
+        }
 
         double posOne = (d1.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(d1.getPortNumber()) : lynxResponse.getEncoder(d1.getPortNumber());
         double posTwo = (d2.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(d2.getPortNumber()) : lynxResponse.getEncoder(d2.getPortNumber());
@@ -54,6 +101,30 @@ public class BulkRead {
         double posFour = (d4.getDirection() == DcMotorSimple.Direction.REVERSE) ? -lynxResponse.getEncoder(d4.getPortNumber()) : lynxResponse.getEncoder(d4.getPortNumber());
 
         return new double[]{posOne, posTwo, posThree, posFour};
+
+    }
+
+    /*public void i2cReadTest() {
+
+        i2cRead.get(1).read()
+
+    }*/
+
+    public class BetterI2cDeviceSynchImplOnSimple extends I2cDeviceSynchImplOnSimple {
+        public BetterI2cDeviceSynchImplOnSimple(I2cDeviceSynchSimple simple, boolean isSimpleOwned) {
+            super(simple, isSimpleOwned);
+        }
+
+        @Override
+        public void setReadWindow(ReadWindow window) {
+            // intentionally do nothing
+        }
+    }
+
+
+    public String handleException(Exception e) {
+
+        return e.getStackTrace().toString();
 
     }
 
